0a44145f4f8e81ea608917d270c3aa078dc90ea5
Ashish Baghel <ashishbaghel10@gmail.com>
2026-04-03 22:59:20 +0530

    Add: meeting-schedule - submission-3

---
+++ Data Structures & Algorithms/meeting-schedule/submission-3.js
@@ -0,0 +1,26 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ *   constructor(start, end) {
+ *     this.start = start;
+ *     this.end = end;
+ *   }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {Interval[]} intervals
+     * @returns {boolean}
+     */
+    canAttendMeetings(intervals) {
+       let  sortedInt = intervals.sort((a,b)=>a.start-b.end);
+        for(let i=1;i<sortedInt.length;i++){
+            if(sortedInt[i-1].end>sortedInt[i].start){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
