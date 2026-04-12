2ce626360979581e362e3f7ce425833f2f5f38c8
Ashish Baghel <ashishbaghel10@gmail.com>
2026-04-12 17:33:56 +0530

    Add: house-robber-ii - submission-0

---
+++ Data Structures & Algorithms/house-robber-ii/submission-0.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    rob(nums) {
+        let n = nums.length;
+        if(n===1) return nums[0];
+        if(n===2) return Math.max(nums[0],nums[1]);
+        
+        return Math.max(this.robHelper(nums.slice(0,-1)),this.robHelper(nums.slice(1)));
+
+    
+    }
+
+
+    robHelper(nums){
+        if(nums.length === 1) return nums[0];
+        let result =[nums[0],Math.max(nums[0],nums[1])];
+        for(let i=2;i<nums.length;i++){
+            result[i] = Math.max(nums[i]+result[i-2],result[i-1]);
+        }
+
+        return result[result.length-1];
+    }
+}
