f0294e8839c2560495d8583bd8b5f2c43becee51
Ashish Baghel <ashishbaghel10@gmail.com>
2026-04-15 18:05:27 +0530

    Add: valid-palindrome-ii - submission-2

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-2.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    validPalindrome(s) {
+        let r=s.length-1;
+        let l=0;
+        let count=0;
+
+        while(l<r){
+            if(s[l] !== s[r]){
+                return (this.isPalindrome(s,l+1,r) || this.isPalindrome(s,l,r-1));
+            };
+            l++;
+            r--;
+        }
+        
+        return true;
+    }
+
+
+    isPalindrome(s,l,r){
+         while(l<r){
+            if(s[l] !== s[r]) return false;
+            l++;
+            r--;
+        }
+        return true
+    }
+}
