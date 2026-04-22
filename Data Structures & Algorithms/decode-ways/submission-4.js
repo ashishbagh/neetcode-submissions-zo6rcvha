9d107c01b75814381bd0f5ca25eb5d4f5f9d0466
Ashish Baghel <ashishbaghel10@gmail.com>
2026-04-22 18:49:51 +0530

    Add: decode-ways - submission-4

---
+++ Data Structures & Algorithms/decode-ways/submission-4.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    numDecodings(s) {
+        let res = 0;
+        let cache = {}
+        const dfs = (i)=>{
+            if(cache[i]) return cache[i];
+            if(i===s.length) return 1;
+            if(i>s.length) return 0;
+            if(s[i]==='0') return 0;
+            res=dfs(i+1);
+            if(s[i] ==='1' || s[i] ==='2' && s[i+1] <'7'){
+                res+=dfs(i+2);
+            }
+            cache[i] =res;
+            return res;
+        }
+
+        return dfs(0);
+    }
+}
