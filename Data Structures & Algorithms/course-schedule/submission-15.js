d59a871e623ade3176151de81d2bd76704e12b34
Ashish Baghel <ashishbaghel10@gmail.com>
2026-04-27 23:23:51 +0530

    Add: course-schedule - submission-15

---
+++ Data Structures & Algorithms/course-schedule/submission-15.js
@@ -0,0 +1,38 @@
+class Solution {
+    /**
+     * @param {number} numCourses
+     * @param {number[][]} prerequisites
+     * @return {boolean}
+     */
+    canFinish(numCourses, prerequisites) {
+        const pre=prerequisites;
+        let adj={};
+        for(let i=0;i<numCourses;i++){
+            adj[i]=[];
+        }
+        for(const [v,m] of pre){
+            adj[v].push(m);
+        }
+
+        let memo={};
+    
+        const dfs=(node,visit)=>{
+          if(memo[node]) return memo[node];
+          if(visit.has(node)) return false;
+          const arr = adj[node];
+          if(arr.length === 0){
+            return true;
+          };
+          visit.add(node);
+          let res = arr.map((item)=>dfs(item,visit));
+          memo[node] = !res.includes(false);
+          return memo[node];
+        }
+        let done = new Set();
+        for(let i=0;i<numCourses;i++){
+            let visit = new Set();
+            if(!dfs(i,visit)) return false;
+        }
+        return true;
+    }
+}
