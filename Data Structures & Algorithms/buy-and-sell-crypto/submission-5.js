e640d9c1d9b3828072e599ee70415c4dc64f25c7
Ashish Baghel <ashishbaghel10@gmail.com>
2026-04-28 19:58:31 +0530

    Add: buy-and-sell-crypto - submission-5

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-5.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let left = 0;
+        let right = 1;
+        let profit = 0;
+        while (right < prices.length) {
+            if (prices[right] > prices[left]) {
+            profit = Math.max(prices[right] - prices[left], profit);
+            } else {
+            left = right;
+            }
+            right++;
+        }
+
+        return profit;
+    }
+}
