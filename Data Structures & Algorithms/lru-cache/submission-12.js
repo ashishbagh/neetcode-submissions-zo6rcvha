205fb96f8bb2acbee5cf9abda782422e43b69304
Ashish Baghel <ashishbaghel10@gmail.com>
2026-05-04 19:36:15 +0530

    Add: lru-cache - submission-12

---
+++ Data Structures & Algorithms/lru-cache/submission-12.js
@@ -0,0 +1,37 @@
+class LRUCache {
+    /**
+     * @param {number} capacity
+     */
+    constructor(capacity) {
+        this.cache= new Map();
+        this.capacity= capacity;
+    }
+
+    /**
+     * @param {number} key
+     * @return {number}
+     */
+    get(key) {
+      if(!this.cache.has(key)) return -1;
+      let val = this.cache.get(key)
+      this.cache.delete(key);
+      this.cache.set(key,val);
+      return this.cache.get(key);
+    }
+
+    /**
+     * @param {number} key
+     * @param {number} value
+     * @return {void}
+     */
+    put(key, value) {
+        if(this.cache.has(key)){
+          this.cache.delete(key,value);
+        }
+        this.cache.set(key,value);
+        if(this.cache.size>this.capacity){
+            let leastRecentUsed = this.cache.keys().next().value;
+            this.cache.delete(leastRecentUsed);
+        }
+    }
+} 
