436d10e02cd7fc938ef1e293bd6d190f4cfe6a9a
Ashish Baghel <ashishbaghel10@gmail.com>
2026-05-05 14:59:14 +0530

    Add: three-integer-sum - submission-14

---
+++ Data Structures & Algorithms/three-integer-sum/submission-14.js
@@ -0,0 +1,40 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums) {
+        nums=nums.sort((a,b)=> a-b);
+        let result=[];
+        let set = new Set();
+    
+        for(let i=0;i<nums.length;i++){
+            if (i > 0 && nums[i] === nums[i - 1]) continue;
+            let l=i+1;
+            let r=nums.length-1;
+            let res=[];
+            while(l<r){
+                let sum=nums[i]+nums[l]+nums[r];
+                if(sum>0){
+                    r--;
+                }else if(sum<0){
+                    l++;
+                }else{
+                    result.push([...[nums[i],nums[l],nums[r]]]);
+                    l++;
+                    r--;
+                    while (l < r && nums[l] === nums[l - 1]) { // remove duplicate
+                    l++;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+     
+    getTwoSum(i,nums){
+       
+
+       return res;
+    }
+}
