160b61071f63e81ff51d1b852bbc1072eaa77149
Ashish Baghel <ashishbaghel10@gmail.com>
2026-05-08 13:55:01 +0530

    Add: buy-and-sell-crypto-with-cooldown - submission-4

---
+++ Data Structures & Algorithms/buy-and-sell-crypto-with-cooldown/submission-4.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+      let memo = new Map();
+      const dfs = (i,buying)=>{
+        if(i>=prices.length){
+            return 0;
+        }
+        let key=`${i},${buying}`;
+        if(memo.has(key)) return memo.get(key);
+        let cooldown =  dfs(i + 1, buying);
+        let ans;
+        if(buying){
+            let buy = dfs(i+1,false)-prices[i];
+            ans = Math.max(buy,cooldown);
+        }else{
+            let sell = dfs(i+2,true)+prices[i];
+            ans = Math.max(sell,cooldown);
+        }
+
+        memo.set(key,ans);
+        return ans;
+      }
+      return dfs(0,true)
+    }
+}
