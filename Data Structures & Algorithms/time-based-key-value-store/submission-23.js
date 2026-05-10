8351b5d8bf6c1295ee3ec1004f54b492ffb49750
Ashish Baghel <ashishbaghel10@gmail.com>
2026-05-11 00:59:15 +0530

    Add: time-based-key-value-store - submission-23

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-23.js
@@ -0,0 +1,43 @@
+class TimeMap {
+    constructor() {
+        this.keyStore = new Map();
+    }
+
+    /**
+     * @param {string} key
+     * @param {string} value
+     * @param {number} timestamp
+     * @return {void}
+     */
+    set(key, value, timestamp) {
+        let memo={};
+        if(this.keyStore.has(key)){
+           memo = this.keyStore.get(key);
+           memo[timestamp]=value;
+        }
+        memo[timestamp]=value;
+        this.keyStore.set(key,memo);
+        return value
+    }
+
+    /**
+     * @param {string} key
+     * @param {number} timestamp
+     * @return {string}
+     */
+    get(key, timestamp) {
+        if(!this.keyStore.has(key)) return "";
+        let memo = this.keyStore.get(key);
+        if(memo[timestamp] !== undefined) return memo[timestamp];
+        let keys = Object.keys(memo);
+        let r = keys.length-1;
+        
+        while(r>=0){
+            if(keys[r]<=timestamp){
+               return memo[keys[r]];
+            }
+            r--;
+        }
+        return "";
+    }
+}
