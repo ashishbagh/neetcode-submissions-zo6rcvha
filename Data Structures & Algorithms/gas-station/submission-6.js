4b3a30cff05cb409f8daa0d2c1d3bfc8dd9ed7f3
Ashish Baghel <ashishbaghel10@gmail.com>
2026-05-13 13:57:22 +0530

    Add: gas-station - submission-6

---
+++ Data Structures & Algorithms/gas-station/submission-6.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} gas
+     * @param {number[]} cost
+     * @return {number}
+     */
+    canCompleteCircuit(gas, cost) {
+        let l=gas.length-1;
+        let  sumGas = gas.reduce((a,b)=>a+b,0);
+        let  sumCost = cost.reduce((a,b)=>a+b,0);
+        if(sumGas<sumCost) return -1;
+
+        let i=0;
+        let total=0;
+        let res = 0;
+
+        while(i <gas.length){
+            total+=(gas[i] - cost[i]);
+            if(total<0){
+                total=0;
+                res=i+1;
+            }
+            i++
+        }
+        return res;
+    }
+}
