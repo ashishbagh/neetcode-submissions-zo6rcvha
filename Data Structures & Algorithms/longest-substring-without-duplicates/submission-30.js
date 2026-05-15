283634884a8bef57e4ae77bde765d91743512784
Ashish Baghel <ashishbaghel10@gmail.com>
2026-05-15 23:27:33 +0530

    Add: longest-substring-without-duplicates - submission-30

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-30.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLongestSubstring(s) {
+        if (!s) {
+         return s.length;
+        }
+        let map=new Map();
+        let r=0;
+        let l=0;
+        let max=1;
+        while(r<s.length){
+            if(map.has(s[r]) && map.get(s[r])>=l){
+                l=map.get(s[r])+1;
+            }
+            map.set(s[r],r);
+            max = Math.max(max,r-l+1);
+            r++
+        }
+        return max;
+    }
+}
