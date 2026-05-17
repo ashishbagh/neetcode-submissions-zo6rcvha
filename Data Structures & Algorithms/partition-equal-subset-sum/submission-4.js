dfd1599d0ef5868295c46031e9405045a9705b32
Ashish Baghel <ashishbaghel10@gmail.com>
2026-05-17 16:13:11 +0530

    Add: partition-equal-subset-sum - submission-4

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-4.js
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    canPartition(nums) {
+        let sum = nums.reduce((a,b)=>a+b,0);
+        if(sum%2 !== 0) return false;
+        let target = Math.floor(sum/2);
+        let count=0;
+        let set = new Set();
+        const dfs = (l,amount)=>{
+            if(set.has(count)) return;
+            if(target===amount){
+                count++;
+                set.add(count)
+                return 1;
+            }
+            if(l>nums.length) return 0;
+            if(target<amount) return 0;
+         
+          for(let i=l;i<nums.length;i++){
+            dfs(i+1,amount+nums[i]);
+          }
+
+        
+         return;
+        }
+        
+        dfs(0,0)
+        console.log(count)
+        return count === 1;
+    }
+}
