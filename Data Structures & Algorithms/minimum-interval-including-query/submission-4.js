de577321520d11f27c5d9df96104511e7ef189bb
Ashish Baghel <ashishbaghel10@gmail.com>
2026-05-24 13:50:44 +0530

    Add: minimum-interval-including-query - submission-4

---
+++ Data Structures & Algorithms/minimum-interval-including-query/submission-4.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {number[][]} intervals
+     * @param {number[]} queries
+     * @return {number[]}
+     */
+    minInterval(intervals, queries) {
+        intervals = intervals.sort((a,b)=>a[0]-b[0]);
+        let result = new Array(queries.length).fill(Infinity);
+        let i=0;
+        for(const query of queries){
+            for(const [start,end] of intervals){
+                 if(start<=query && query<=end ){
+                    let res = end-start+1;
+                    result[i] = Math.min(result[i],res);
+                 }
+           }
+           if(result[i] === Infinity) result[i]=-1;
+           i++;
+        }
+        // let r=0
+        // while(r<result.length){
+        //     if(result[r] === Infinity) result[r]=-1;
+        //     r++
+        // }
+        
+        return result;
+    }
+}
