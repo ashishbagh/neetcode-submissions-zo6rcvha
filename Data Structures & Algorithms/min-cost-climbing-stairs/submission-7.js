1ac0ac2051f46f3431f10f28f6d558d3a0f95d11
Ashish Baghel <ashishbaghel10@gmail.com>
2026-06-13 13:49:58 +0530

    Add: min-cost-climbing-stairs - submission-7

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-7.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[]} cost
+     * @return {number}
+     */
+    minCostClimbingStairs(cost) {
+       let one = 0;
+       let two = 0;
+        for (let i = cost.length - 1; i >= 0; i--) {
+            let curr = cost[i] + Math.min(one, two);
+            two = one;
+            one = curr;
+        }
+
+        return Math.min(one, two);
+    }
+}
