fa96800202cbdaa9ff85cd38577a54dfb1f51fcb
Ashish Baghel <ashishbaghel10@gmail.com>
2026-06-18 00:13:21 +0530

    Add: longest-increasing-subsequence - submission-20

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-20.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    lengthOfLIS(nums) {
+        let tails=[];
+        for(const num of nums){
+            let left=0;
+            let right=tails.length;
+            while(left<right){
+                let mid = parseInt((left+right)/2);
+                if(tails[mid]<num){
+                    left=mid+1
+                }else{
+                    right=mid;
+                }
+            }
+
+            tails[left]=num;
+        }
+
+        return tails.length;
+
+    }
+}
