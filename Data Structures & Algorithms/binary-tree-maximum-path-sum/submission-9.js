093799fc8e47b2989d0c20f3b7ea2c78b057d799
Ashish Baghel <ashishbaghel10@gmail.com>
2026-07-20 13:23:01 +0530

    Add: binary-tree-maximum-path-sum - submission-9

---
+++ Data Structures & Algorithms/binary-tree-maximum-path-sum/submission-9.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number}
+     */
+    maxPathSum(root) {
+        let max = -Infinity;
+        if(root.val && !root.right && !root.left){
+            return root.val;
+        }
+    
+        const dfs = (node)=>{
+            if(!node) return 0;
+            let left = Math.max(dfs(node.left),0);
+            let right = Math.max(dfs(node.right),0);
+            let sum = node.val + left + right;
+
+            max = Math.max(sum,max)
+            return node.val+Math.max(left,right);
+        }
+
+        dfs(root)
+       
+        return max;
+    }
+}
