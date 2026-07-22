0203c59f5afcd87ddb137fee8d2414ffea913e41
Ashish Baghel <ashishbaghel10@gmail.com>
2026-07-22 21:23:05 +0530

    Add: eating-bananas - submission-15

---
+++ Data Structures & Algorithms/eating-bananas/submission-15.js
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {number[]} piles
+     * @param {number} h
+     * @return {number}
+     */
+    minEatingSpeed(piles, h) {
+      let left=1;
+      let right=Math.max(...piles);
+      let result=right;
+
+        let mid=parseInt((left+right)/2);
+        console.log(mid);
+
+        const speed=(k)=>{
+            let res=0;
+            for(let pile of piles){
+                res+=Math.ceil(pile/k);
+            }
+            return res<=h;
+        }
+
+      while(left<=right){
+        let mid=parseInt((left+right)/2);
+        if(speed(mid)){
+          result = Math.min(result,mid);
+          right=mid-1;
+        }else{
+          left=mid+1;
+        }
+      }
+      return result;
+    }
+}
