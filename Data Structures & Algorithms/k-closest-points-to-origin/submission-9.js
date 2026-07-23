109d40c7619a44a790437ca9527d5f4a59383a51
Ashish Baghel <ashishbaghel10@gmail.com>
2026-07-23 11:11:44 +0530

    Add: k-closest-points-to-origin - submission-9

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-9.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[][]} points
+     * @param {number} k
+     * @return {number[][]}
+     */
+    kClosest(points, k) {
+        const dist = (point) => point[0] * point[0] + point[1] * point[1];
+        
+        
+
+
+        points.sort((a,b)=>dist(a)-dist(b))
+
+        return points.slice(0,k);
+    }
+
+
+    
+}
